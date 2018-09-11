de57f34c3c3cd682297a4cf56178470646ddfeed
Narut Poovorakit <narutp@Naruts-MacBook-Pro.local>
2018-09-11 13:37:58 +0700

    Init project

---
+++ iOS-Swift-Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Swift-Practice
+//
+//  Created by Narut Poovorakit on 11/9/2561 BE.
+//  Copyright © 2561 Narut Poovorakit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
